d15ea4b20e1900014e2866757ab31df6f564b0d6
Spandamn <spandan.punwatkar@gmail.com>
2020-03-03 22:57:16 +0530

    Add join/leave commands

--- Commands.java
+++ Commands.java
@@ -150,7 +150,7 @@ public class Commands {
 			return;
 		}
 		if (IO.toId(mess).length() == 0 && ob != null) {
-			ob.leave();
+			bot.sendToServer("/leave " + ob.id);
 			return;
 		}
 		String rooms[] = mess.split(",");
